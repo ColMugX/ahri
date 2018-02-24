e10100a9aacf81e5a438bd0135b390a91d11bc11
ColMugX <colmugx@qq.com>
2018-02-25 00:56:33 +0800

    add unit test: checkmodel.

--- packages/ahri/index.js
+++ packages/ahri/index.js
@@ -1,3 +1,2 @@
 export { default as connect } from './src/connect'
 export { default } from './src'
-
---
+++ packages/ahri/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'moduleFileExtensions': [
+    'js'
+  ],
+  'transform': {
+    '^.+\\.js$': 'babel-jest'
+  },
+  'testMatch': ['<rootDir>/test/**/?(*.)(spec|test).js?(x)']
+}
