b4959d3b45deea0b3beed13325072c2062d05fdc
ColMugX <colmugx@qq.com>
2018-03-06 19:02:34 +0800

    update connect.

--- packages/ahri/src/connect.js
+++ packages/ahri/src/connect.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 
-const functional = (fn) => {
-  return Vue.component(fn.name || 'anonymous', {
+export const connect = mapToOpt => {
+  if (mapToOpt === 'functional') {
+    return component => functional(component)
+  }
+}
+
+const functional = component => {
+  return Vue.component(component.name || 'anonymous', {
     functional: true,
     render (h, ctx) {
-      return fn.call(this, h, ctx.props, ctx.data)
+      const store = findStore(ctx.parent)
+      const props = { ...store, ...ctx.props }
+      return component.call(this, h, props, ctx.data)
     }
   })
 }
 
-export {
-  functional
-}
+const findStore = child => child.$store ? child.$store : findStore(child.$parent)
