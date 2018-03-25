2801470af26925a9c80c2ea04962b4eeed4cadd9
ColMugX <colmugx@qq.com>
2018-03-25 23:04:30 +0800

    transform by babel.

--- packages/ahri/index.js
+++ packages/ahri/index.js
@@ -1,6 +1,21 @@
+'use strict';
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-module.exports = require("./lib");
-exports.connect = require("./lib/connect");
+exports.default = require('./lib');
+module.exports = exports['default'];
+
+var _connect = require('./lib/connect');
+
+Object.keys(_connect).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _connect[key];
+    }
+  });
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
